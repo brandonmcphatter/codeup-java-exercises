9d9b666d541402abaa13bf48498df00138e22060
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-05-12 16:59:34 -0500

    Syntax, Types, Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,30 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is Codeup!");
+        int myFavoriteNumber = 24;
+        System.out.println(myFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+//        long myNumber;
+//        myNumber = 123;
+//        System.out.println(myNumber);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+//        String class;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+
+
+
+
     }
 }
