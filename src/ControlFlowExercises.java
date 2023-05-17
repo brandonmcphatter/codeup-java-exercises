696e40fcf505306178f59546311918ce758a17a1
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-05-17 09:53:33 -0500

    updated

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -41,6 +41,10 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+//        for (long i = 2; i < 1000000; i * i){
+//            System.out.println(i);
+//        }
+
 //        #2
 
 //        for (int i = 1; i <= 100; i++){
@@ -58,45 +62,53 @@ public class ControlFlowExercises {
 //        #3
 
 //        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter an integer:");
-//        String userInteger = sc.nextLine();
-//        int userNumber = Integer.parseInt(userInteger);
-//        System.out.println("Here is your table!");
-//        System.out.println("number | squared | cubed");
-//        System.out.println("------ | ------- | -----");
-//        for (int i = 1; i <= userNumber; i++){
-//            int squared = i * i;
-//            int cubed = i * i * i;
-//            System.out.printf("%-6d | %-7d | %-5d %n", i, squared, cubed);
-//
-//        }
+//        boolean confirm = true;
+//        do {
+//            System.out.println("Enter an integer:");
+//            String userInteger = sc.nextLine();
+//            int userNumber = Integer.parseInt(userInteger);
+//            System.out.println("Here is your table!");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for (int i = 1; i <= userNumber; i++){
+//                int squared = i * i;
+//                int cubed = i * i * i;
+//                System.out.printf("%-6d | %-7d | %-5d %n", i, squared, cubed);
+//            }
+//            System.out.println("Continue? [y/n]");
+//            String userAnswer = sc.nextLine();
+//            if (userAnswer.equals("n")){
+//                confirm = false;
+//                System.out.println("Thanks for your participation!");
+//            }
+//        } while (confirm);
 
 //        #4
 
-        Scanner sc = new Scanner(System.in);
-        boolean confirm = true;
-        do {
-            System.out.println("What is your grade? (0-100):");
-            int userGrade = Integer.parseInt(sc.nextLine());
-            if (userGrade >= 88){
-                System.out.println("You got an A!");
-            } else if (userGrade >= 80 & userGrade <= 87) {
-                System.out.println("You got a B");
-            } else if (userGrade >= 67 & userGrade <=79) {
-                System.out.println("You got a C");
-            } else if (userGrade >= 60 & userGrade <= 66){
-                System.out.println("You got a D");
-            } else {
-                System.out.println("You got got an F :(");
-            }
-
-            System.out.println("Continue? [y/n]");
-            String userAnswer = sc.nextLine();
-
-            if (userAnswer.equals("n")){
-                confirm = false;
-                System.out.println("Thanks for your participation!");
-            }
-        } while (confirm);
+//        Scanner sc = new Scanner(System.in);
+//        boolean confirm = true;
+//        do {
+//            System.out.println("What is your grade? (0-100):");
+//            int userGrade = Integer.parseInt(sc.nextLine());
+//            if (userGrade >= 88){
+//                System.out.println("You got an A!");
+//            } else if (userGrade >= 80) {
+//                System.out.println("You got a B");
+//            } else if (userGrade >= 67) {
+//                System.out.println("You got a C");
+//            } else if (userGrade >= 60) {
+//                System.out.println("You got a D");
+//            } else {
+//                System.out.println("You got got an F :(");
+//            }
+//
+//            System.out.println("Continue? [y/n]");
+//            String userAnswer = sc.nextLine();
+//
+//            if (userAnswer.equals("n")){
+//                confirm = false;
+//                System.out.println("Thanks for your participation!");
+//            }
+//        } while (confirm);
     }
 }
