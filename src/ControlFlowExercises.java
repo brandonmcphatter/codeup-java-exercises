e031308c4b14790875a758c0b41b093e955d1202
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-05-17 09:10:19 -0500

    updated #1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,7 +7,7 @@ public class ControlFlowExercises {
 
 //        int i = 5;
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.print(i + " ");
 //            i++;
 //        }
 
