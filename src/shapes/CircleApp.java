01237817e73dc074154a7f8fde7590a5177bb6f4
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-05-22 16:56:20 -0500

    shortened decimal places

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,18 +2,20 @@ package shapes;
 
 import util.Input;
 
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class CircleApp {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public static void main(String[] args) {
 
         Input circInput = new Input();
         int circValue = circInput.getInt();
         Circle firstCircle = new Circle(circValue);
-        System.out.println("Radius for firstCircle is " + circValue);
-        System.out.println("Circumference is " + firstCircle.getCircumference());
-        System.out.println("Area is " + firstCircle.getArea());
+        System.out.println("Radius for firstCircle is " + df.format(circValue));
+        System.out.println("Circumference is " + df.format(firstCircle.getCircumference()));
+        System.out.println("Area is " + df.format(firstCircle.getArea()));
         System.out.println(firstCircle.getRadius());
     }
 }
